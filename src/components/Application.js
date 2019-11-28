1739c9756478f6907cb090fce6dca75722278907
Russell Cape <russellcape98@gmail.com>
2019-11-28 16:23:46 -0500

    Applicaton page (no change)

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,46 +6,6 @@ import DayList from "components/DayList";
 import Appointment from "components/Appointment"
 import { getAppointmentsForDay, getInterview } from "../helpers/selectors"
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Russell Cape",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
-
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: "monday",
