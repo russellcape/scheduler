6d8200707842f28f1353fd4f1e1908c6a5f1fd87
Russell Cape <russellcape98@gmail.com>
2019-11-28 16:24:49 -0500

    Started working on custom hooks

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,25 @@
+// import { useState } from "react"
+
+// export default function useApplicationData(state, applicationData) {
+
+// const useApplicationData = {
+//   state: {},
+//   setDay: [],
+//   bookInterview: ((event) => {
+//     applicationData
+//   }),
+//   cancelInterview
+// }
+
+// }
+
+
+// // Our useApplicationData Hook will return an object with four keys.
+
+// // The state object will maintain the same structure.
+// // The setDay action can be used to set the current day.
+// // The bookInterview action makes an HTTP request and updates the local state.
+// // The cancelInterview action makes an HTTP request and updates the local state.
+
+
+// // ... wrong day
